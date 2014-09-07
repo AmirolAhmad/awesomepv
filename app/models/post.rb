43a96a51de6b10fffd5f75d5fd4e173996e97957
AmirolAhmad <9w2pdf@gmail.com>
2014-09-07 18:55:41 +0800

    1 more post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
 
 	belongs_to :user
 
-	self.per_page = 26
+	self.per_page = 27
 
 	DISQUS_SHORTNAME = Rails.env == "development" ? "awesomevideos".freeze : "awesomevideos".freeze
 
