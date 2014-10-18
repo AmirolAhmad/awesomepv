ea3dfe7bc0e2a083354c5a6ec510526098f7cb3b
AmirolAhmad <9w2pdf@gmail.com>
2014-10-19 00:55:16 +0800

    added v2.0 files

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :omniauthable and :registerable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :posts, dependent: :destroy
-end
