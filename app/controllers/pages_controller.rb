28b8ae2b969284f4f352022c654d1ad5d2886617
AmirolAhmad <9w2pdf@gmail.com>
2014-10-21 00:57:17 +0800

    add featured & publish to index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,10 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       format.html {
-        @carousel = @videos.published.featured.take(5)
+        @carousel = @videos.published.featured.take(7)
   	    @published_only = @videos.published_only.take(7)
   	    @sub = @published_only.shift(4)
+        @both = @carousel.shift(4)
       }
       format.js {
         @videos = @videos.published_only.take(7)
