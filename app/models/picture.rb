4ee643802bc032b3b35c4fb32cc75715ac923459
AmirolAhmad <9w2pdf@gmail.com>
2014-10-22 13:27:12 +0800

    validation for picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,25 @@
 class Picture < ActiveRecord::Base
+  acts_as_paranoid
+
+  extend FriendlyId #https://github.com/norman/friendly_id
+
+  friendly_id :pid, use: [:slugged, :finders, :history]
+
+  belongs_to :user, foreign_key: :user_id
+
+  validates :title, length: { maximum: 240 }, presence: true
+
+  self.per_page = 12
+
+  default_scope -> { order('pictures.created_at DESC') }
+  scope :published, -> { where('state = ?', 'published').unscope(:order).order('`pictures`.published_at DESC') }
+  scope :featured, -> { where(featured: true) }
+  scope :published_only, -> { where('featured IS NOT TRUE') }
+  scope :random, -> { unscope(:order).order('RAND()') }
+
+  protected
+
+  def published?
+    state == 'published'
+  end
 end
